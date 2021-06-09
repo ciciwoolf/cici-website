6e22fecde751e7d044aad2f42895dee46840b309
Cici Woolf <60899330+ciciwoolf@users.noreply.github.com>
2021-06-09 08:56:38 -0500

    Added images

---
+++ cici-website/README.md
@@ -0,0 +1,2 @@
+#www.theartofciciwoolf.com
+
---
+++ cici-website/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          The Art of Cici Woolf - Under Construction
+        </p>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
